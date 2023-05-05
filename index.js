c779489de2b3c231cfa4790778211862e8239462
Rush Burriel <burrielrush@gmail.com>
2023-05-05 12:14:17 -0600

    updated code to render correct color and shape of logo

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const SVG = require('./svg');
 const { Square, Triangle, Circle } = require('./lib/shapes.js');
 const shape = require('./lib/shapes.js');
 
 
+class SVG {
+    constructor(shapeHTML) {
+        this.textElement = "";
+        this.shapeElement = "";
+        this.shapeHTML = shapeHTML;
+    }
+
+    render() {
+        return `<svg version="1.1" width="300" height="200" 
+        xmlns="http://www.w3.org/2000/svg">${this.shapeHTML}</svg>`;
+} 
+
+setText(message, color) {
+    if (message.length > 3) {
+        throw new Error("Message must be less than 3 characters");
+    }
+    this.textElement = `,text x="150" y="150" fill="${color}" font-size="20">${message}</text`;
+}
+
+setShape(shape) {
+    this.shapeElement = shape.render();
+    }
+}
 
 const questions = [
     {
@@ -15,7 +37,7 @@ const questions = [
     {
         type: "input",
         name: "textColor",
-        message: "Enter color of text:",
+        message: "Enter color of shape:",
     },
     {
         type: "checkbox",
@@ -30,7 +52,7 @@ const questions = [
     {
         type: "input",
         name: "color",
-        message: "Color of shape:",
+        message: "Color of text:",
     }
 ];
 
@@ -38,17 +60,24 @@ function generateLogo(data) {
     const svg = new SVG();
   
     let shape;
+    console.log(data)
   
-    if (data.shapes.includes('Triangle')) {
+    if (data.shapes.includes('triangle')) {
       shape = new Triangle(data.textColor, data.characters, data.color);
-    } else if (data.shapes.includes('Square')) {
+    } else if (data.shapes.includes('square')) {
       shape = new Square(data.textColor, data.characters, data.color);
-    } else if (data.shapes.includes('Circle')) {
+    } else if (data.shapes.includes('circle')) {
       shape = new Circle(data.textColor, data.characters, data.color);
     } 
+
+    console.log(shape)
+    console.log(shape.render())
+    const newsvg = new SVG(shape.render());
+    console.log(newsvg)
+    console.log(newsvg.render())
  
 
-    return svg.toString();
+    return newsvg.render();
   }
   
 
